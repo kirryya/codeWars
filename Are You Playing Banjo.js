b04ff8aed69ab8fc82111f0c6858ce4646549a59
kirryya <kir1987@inbox.ru>
2022-01-06 12:31:09 +0300

    repeat

---
+++ Are You Playing Banjo.js
@@ -0,0 +1,7 @@
+function areYouPlayingBanjo(name) {
+    if (name.toUpperCase().startsWith("R")) {
+      return name + " plays banjo";
+  } else{
+    return name + " does not play banjo";
+    }
+  }
